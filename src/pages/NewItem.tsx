1af519ea8e7ce35166c0374629c6e67ffc84a5f0
Ryan Hedges <ryanhedges@gmail.com>
2020-01-13 16:44:53 -0800

    Add Android and add photo feature

--- src/pages/NewItem.tsx
+++ src/pages/NewItem.tsx
@@ -1,15 +1,29 @@
 import { 
     IonBackButton,
     IonButtons,
+    IonButton,
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
     IonToolbar
 } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { IonFabButton } from '@ionic/react';
+import { Plugins, CameraResultType } from '@capacitor/core';
+import { async } from 'q';
 
 const NewItem: React.FC = () => {
+    const { Camera } = Plugins;
+    const [photo, setPhoto] = useState();
+    const takePhoto = async () => {
+        const image = await Camera.getPhoto({
+            quality: 90,
+            allowEditing: true,
+            resultType: CameraResultType.Uri
+        })
+        setPhoto(image.webPath)
+    };
     return (
         <IonPage>
             <IonHeader>
@@ -20,7 +34,10 @@ const NewItem: React.FC = () => {
                     <IonTitle>New Item</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent></IonContent>
+            <IonContent className="ion-padding">
+                <img src={photo} />
+                <IonButton onClick={takePhoto}>Take Photo</IonButton>
+            </IonContent>
         </IonPage>
     );
 };
